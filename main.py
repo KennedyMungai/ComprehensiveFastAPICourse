08a7a8cd77503322ae70857acdc9ee4764a7f532
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-24 10:44:38 +0300

    Added a doc string to the root async function

--- main.py
+++ main.py
@@ -2,3 +2,15 @@ from fastapi import FastAPI
 
 
 app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    """This is an asynchronous function that functions as the root of the API
+
+    Returns:
+        JSON: Returns your typical JSON string although it is hardcoded
+    """
+    return {
+        "message": "Hello World"
+    }
