4a92681cd5979facdca8b4c5401aeedd2e69d3d5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-25 15:06:38 +0300

    Added some insert sql queries

---
+++ hello_sql.sql
@@ -0,0 +1,5 @@
+INSERT INTO Posts (title, content)
+VALUES(
+        "The first title",
+        " pariatur voluptatem officia. Sed iure odio eaque, quidem sunt officiis quam debitis, inventore enim sequi tempore, aperiam ducimus accusantium ullam. Veritatis vel porro reiciendis voluptatum repellendus autem incidunt quidem?"
+    );
