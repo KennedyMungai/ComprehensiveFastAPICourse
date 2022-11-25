6eb02cafbaed48abe4cb53077612639ca1c9c159
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-25 12:42:23 +0300

    Deleted all the dql commands from the sql file

--- hello_sql.sql
+++
@@ -1,8 +0,0 @@
--- CREATE TABLE products (
---     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
---     product_name VARCHAR(100) NOT NULL,
---     price DECIMAL NOT NULL
--- );
-select *
-FROM products
-ORDER BY id ASC;
