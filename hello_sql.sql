27b983eccbb49795809022dcc8835d4924729f5d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-25 12:57:54 +0300

    Performed a data insert

--- hello_sql.sql
+++ hello_sql.sql
@@ -1,2 +1,7 @@
-ALTER TABLE products
-add COLUMN inventory int NOT NULL
+INSERT INTO products (product_name, price, is_sale, inventory)
+VALUES (
+        'Twitter',
+        '185990',
+        '1',
+        569
+    );
