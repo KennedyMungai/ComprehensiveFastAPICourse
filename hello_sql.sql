1ad92885d3a8a2daf30981a7c4cdba2651ed53b8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-25 12:37:38 +0300

    Commmented cthe create table querry and added a select all items fom the table query

--- hello_sql.sql
+++ hello_sql.sql
@@ -1,5 +1,8 @@
-CREATE TABLE products (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    product_name VARCHAR(100) NOT NULL,
-    price DECIMAL NOT NULL
-);
+-- CREATE TABLE products (
+--     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--     product_name VARCHAR(100) NOT NULL,
+--     price DECIMAL NOT NULL
+-- );
+select *
+FROM products
+ORDER BY id ASC;
