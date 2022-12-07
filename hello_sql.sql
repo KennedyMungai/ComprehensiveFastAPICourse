a5b1267d2f1cd05fe142d0b9fbfe7f856aca3a7c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-07 09:40:12 +0300

    Added a select all query

--- hello_sql.sql
+++ hello_sql.sql
@@ -9,3 +9,10 @@ VALUES(
         "The first title",
         " pariatur voluptatem officia. Sed iure odio eaque, quidem sunt officiis quam debitis, inventore enim sequi tempore, aperiam ducimus accusantium ullam. Veritatis vel porro reiciendis voluptatum repellendus autem incidunt quidem?"
     );
+-- ------------------------------------------
+-- ------------------------------------------
+-- Select all the data from the Posts table
+-- ------------------------------------------
+-- ------------------------------------------
+SELECT *
+FROM `Posts`
